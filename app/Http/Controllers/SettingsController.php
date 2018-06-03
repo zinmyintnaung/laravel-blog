fbb41493013a353c6ec2b3a987c80e1cc29c1f82
Zin Myint Naung <zinmyintnaung@gmail.com>
2018-06-03 19:44:19 +0800

    setting updated

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Setting;
+use Session;
 
 class SettingsController extends Controller
 {
@@ -17,7 +18,21 @@ class SettingsController extends Controller
     }
 
     public function update(){
-        dd(request());
+        //dd(request());
+        $this->validate(request(), [
+            'site_name' => 'required',
+            'contact_number' => 'required',
+            'contact_email' => 'required',
+            'address' => 'required'
+        ]);
         $settings = Setting::first();
+        $settings->site_name = request()->site_name;
+        $settings->contact_number = request()->contact_number;
+        $settings->contact_email = request()->contact_email;
+        $settings->address = request()->address;
+        $settings->save();
+        Session::flash('success', 'Settings updated!');
+        return redirect()->back();
+
     }
 }
