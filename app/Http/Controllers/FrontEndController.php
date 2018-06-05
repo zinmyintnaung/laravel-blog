58c33bc6abbb50acd85446d242460a3a54eeb048
Zin Myint Naung <zinmyintnaung@gmail.com>
2018-06-05 18:24:34 +0800

    front end

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Setting;
+use App\Category;
+use App\Post;
+
+class FrontEndController extends Controller
+{
+    public function index(){
+        return view('index')
+            ->with('settings', Setting::first())
+            ->with('categories', Category::take(5)->get())
+            ->with('first_post', Post::orderBy('created_at', 'desc')->first())
+            ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
+            ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+            ->with('html', Category::find(1))
+            ->with('css', Category::find(2))
+            ->with('js', Category::find(3));
+    }
+}
