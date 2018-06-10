43deb7e7df22cac30b94723408b0d8cb1c22ec59
Zin Myint Naung <zinmyintnaung@gmail.com>
2018-06-10 20:59:52 +0800

    disqus installed

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Setting;
 use App\Category;
 use App\Post;
+use App\Tag;
 
 class FrontEndController extends Controller
 {
@@ -24,9 +25,18 @@ class FrontEndController extends Controller
     public function singlePost($slug)
     {
         $post = Post::where('slug', $slug)->first();
+        
+        //getting next post
+        $next_id = Post::where('id', '>', $post->id)->min('id');
+
+        $prev_id = Post::where('id', '<', $post->id)->max('id');
+
         return view('single')->with('post', $post)
                              ->with('title', $post->title)
                              ->with('settings', Setting::first())
-                             ->with('categories', Category::take(5)->get());
+                             ->with('categories', Category::take(5)->get())
+                             ->with('next', Post::find($next_id))
+                             ->with('prev', Post::find($prev_id))
+                             ->with('tags', Tag::all());
     }
 }
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -58,7 +58,6 @@
                                         @foreach($post->tags as $tag)
                                             <a href="#" class="w-tags-item">{{ $tag->tag }}</a>
                                         @endforeach
-                                        
                                        
                                     </div>
                                 </div>
@@ -89,7 +88,7 @@
                     <div class="blog-details-author">
 
                         <div class="blog-details-author-thumb">
-                            <img src="app/img/blog-details-author.png" alt="Author">
+                            <img src="{{asset('app/img/blog-details-author.png')}}" alt="Author">
                         </div>
 
                         <div class="blog-details-author-content">
@@ -103,19 +102,19 @@
                             <div class="socials">
 
                                 <a href="#" class="social__item">
-                                    <img src="app/svg/circle-facebook.svg" alt="facebook">
+                                    <img src="{{asset('app/svg/circle-facebook.svg')}}" alt="facebook">
                                 </a>
 
                                 <a href="#" class="social__item">
-                                    <img src="app/svg/twitter.svg" alt="twitter">
+                                    <img src="{{asset('app/svg/twitter.svg')}}" alt="twitter">
                                 </a>
 
                                 <a href="#" class="social__item">
-                                    <img src="app/svg/google.svg" alt="google">
+                                    <img src="{{asset('app/svg/google.svg')}}" alt="google">
                                 </a>
 
                                 <a href="#" class="social__item">
-                                    <img src="app/svg/youtube.svg" alt="youtube">
+                                    <img src="{{asset('app/svg/youtube.svg')}}" alt="youtube">
                                 </a>
 
                             </div>
@@ -124,25 +123,31 @@
 
                     <div class="pagination-arrow">
 
-                        <a href="#" class="btn-prev-wrap">
-                            <svg class="btn-prev">
-                                <use xlink:href="#arrow-left"></use>
-                            </svg>
-                            <div class="btn-content">
-                                <div class="btn-content-title">Next Post</div>
-                                <p class="btn-content-subtitle">Claritas Est Etiam Processus</p>
-                            </div>
-                        </a>
+                        
 
-                        <a href="#" class="btn-next-wrap">
-                            <div class="btn-content">
-                                <div class="btn-content-title">Previous Post</div>
-                                <p class="btn-content-subtitle">Duis Autem Velius</p>
-                            </div>
-                            <svg class="btn-next">
-                                <use xlink:href="#arrow-right"></use>
-                            </svg>
-                        </a>
+                        @if($prev)
+                            <a href="{{ route('post.single', ['slug' => $prev->slug]) }}" class="btn-next-wrap">
+                                <div class="btn-content">
+                                    <div class="btn-content-title">Next Post</div>
+                                    <p class="btn-content-subtitle">{{ $prev->title }}</p>
+                                </div>
+                                <svg class="btn-next">
+                                    <use xlink:href="#arrow-right"></use>
+                                </svg>
+                            </a>
+                        @endif
+
+                        @if($next)
+                            <a href="{{ route('post.single', ['slug' => $next->slug]) }}" class="btn-prev-wrap">
+                                <svg class="btn-prev">
+                                    <use xlink:href="#arrow-left"></use>
+                                </svg>
+                                <div class="btn-content">
+                                    <div class="btn-content-title">Previous Post</div>
+                                    <p class="btn-content-subtitle">{{ $next->title }}</p>
+                                </div>
+                            </a>
+                        @endif
 
                     </div>
 
@@ -156,16 +161,11 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="row">
-
-                    </div>
-
-
+                    @include('includes.disqus')
                 </div>
 
                 <!-- End Post Details -->
-
+                <br>
                 <!-- Sidebar-->
 
                 <div class="col-lg-12">
@@ -180,15 +180,9 @@
                             </div>
 
                             <div class="tags-wrap">
-                                <a href="#" class="w-tags-item">SEO</a>
-                                <a href="#" class="w-tags-item">Advertising</a>
-                                <a href="#" class="w-tags-item">Business</a>
-                                <a href="#" class="w-tags-item">Optimization</a>
-                                <a href="#" class="w-tags-item">Digital Marketing</a>
-                                <a href="#" class="w-tags-item">Social</a>
-                                <a href="#" class="w-tags-item">Keyword</a>
-                                <a href="#" class="w-tags-item">Strategy</a>
-                                <a href="#" class="w-tags-item">Audience</a>
+                                @foreach($tags as $tag)
+                                    <a href="#" class="w-tags-item">{{$tag->tag}}</a>
+                                @endforeach
                             </div>
                         </div>
                     </aside>
